bbeafef1ce08d39ec01e076bf297a77fbcb4d452
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2021-12-24 19:16:02 -0300

    Adicionado tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_web_clone/screens/home_page.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       title: 'WhatsApp Web Clone',
+      home: HomePage(),
     ),
   );
 }
