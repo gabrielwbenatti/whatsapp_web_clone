c9bb5bce0ac16929f6caa9edf2181e9a66919854
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2021-12-25 12:38:55 -0300

    incluido trmas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:whatsapp_web_clone/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:whatsapp_web_clone/rotas.dart';
+import 'package:whatsapp_web_clone/utils/color_pallete.dart';
+
+final ThemeData temaPadrao = ThemeData(
+  appBarTheme: const AppBarTheme(
+    color: ColorPalette.clGreen,
+  ),
+);
 
 void main() async {
   await Firebase.initializeApp(
@@ -11,8 +16,9 @@ void main() async {
   );
 
   runApp(
-    const MaterialApp(
+    MaterialApp(
       title: 'WhatsApp Web Clone',
+      theme: temaPadrao,
       // home: LoginScreen(),
       initialRoute: '/login',
       onGenerateRoute: Rotas.gerarRota,
