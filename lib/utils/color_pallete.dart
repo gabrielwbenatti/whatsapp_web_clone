e24d046f779f7bdfa6fe1ad5d32dd79a8f6d9ab4
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2021-12-25 12:38:59 -0300

    Update color_pallete.dart

--- lib/utils/color_pallete.dart
+++ lib/utils/color_pallete.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   static const Color clWhite = Color(0xFFD9DbD5);
   static const Color clGreen = Color(0xFF075E54);
+  static const Color clGreenBrigth = Color(0xFF25D366);
 }
